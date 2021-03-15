b696788a6318f05fbb1d011f329cdf0bef0cae0e
Fabien Parent <fparent@baylibre.com>
2021-03-15 12:51:11 +0100

    rity: config: use uaccess tag instead of plugdev group
    It is not recommended to rely on groups for HW access. Use the
    uaccess tag instead.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- rity/config.py
+++ rity/config.py
@@ -30,23 +30,10 @@ class Config:
         print_check('udev rules', mtk_rules.exists(),
             "In order for your host machine to be able to talk to the board "
             "through USB without needing root privileges, you need to create "
-            "a udev rules that will grant the plugdev group access "
-            "to your device:\n"
-            """\t$ echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0e8d", ATTR{idProduct}=="201c", MODE="0660", GROUP="plugdev"\\nSUBSYSTEM=="usb", ATTR{idVendor}=="0403", MODE="0660", GROUP="plugdev"' | sudo tee -a """ + Config.UDEV_FILEPATH + "\n"
+            "a udev rules that will grant user access to your device:\n"
+            """\t$ echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0e8d", ATTR{idProduct}=="201c", MODE="0660", TAG+="uaccess"\\nSUBSYSTEM=="usb", ATTR{idVendor}=="0403", MODE="0660", TAG+="uaccess"' | sudo tee -a """ + Config.UDEV_FILEPATH + "\n"
             "\t$ sudo udevadm control --reload-rules\n"
             "\t$ sudo udevadm trigger")
-
-    def check_plugdev_grp(self):
-        user = pwd.getpwuid(os.getuid())
-        plugdev = grp.getgrnam('plugdev')
-        print_check('plugdev group', user.pw_name in plugdev.gr_mem,
-            "In order to be able to talk to the SoC, your user must be in "
-            "the 'plugdev' group.\n"
-            "Please run the following command to join "
-            "the 'plugdev' group:\n"
-            "\t$ sudo usermod -a -G plugdev $USER\n"
-            "This command requires you to log out and log back in to your "
-            "account to be in effect.")
 
     def check(self):
         print_check('fastboot', which('fastboot'),
@@ -55,4 +42,3 @@ class Config:
             "\thttps://developer.android.com/studio/releases/platform-tools")
         if platform.system() == 'Linux':
             self.check_udev_rules()
-            self.check_plugdev_grp()
