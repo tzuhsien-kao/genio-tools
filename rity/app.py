e767983c6e2825e70bd0387c174ac3f333c07819
Fabien Parent <fparent@baylibre.com>
2021-04-09 19:36:34 +0200

    rity: add option to show version
    Add a command line to show the rity-tools version.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- rity/app.py
+++ rity/app.py
@@ -4,7 +4,10 @@
 
 import argparse
 import logging
+import sys
 import textwrap
+
+import rity
 
 class App: # pylint: disable=too-few-public-methods
     """ Common code for RITY tools"""
@@ -17,11 +20,17 @@ class App: # pylint: disable=too-few-public-methods
         )
         self.parser.add_argument('-v', '--verbose', action="store_true",
             help='Enable verbose output')
+        self.parser.add_argument('-V', '--version', action="store_true",
+            help='Show the version')
 
         logging.basicConfig(level=logging.INFO)
 
     def execute(self):
         args = self.parser.parse_args()
+
+        if args.version:
+            print(rity.version)
+            sys.exit(0)
 
         if args.verbose:
             logging.basicConfig(level=logging.DEBUG)
