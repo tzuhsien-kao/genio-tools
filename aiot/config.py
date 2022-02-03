tzuhsien-kao/genio-tools

commit 04ea06ce6f56778c4e6682ad9297a8a325e5be9a

Flashing documentation about the required Linux group
Enhanced the documentation and added a check in aiot-config concerning
the following error:
Opening /dev/ttyACM0 using baudate=115200
open(/dev/ttyACM0): Permission denied
Failed to connect to bootrom: -13

Signed-off-by: David Bouyeure <dbouyeure@baylibre.com>

aiot/config.py
@@ -5,7 +5,7 @@
 from pathlib import Path
 from shutil import which
 import hashlib
-import platform
+import platform, os, grp
 
 import aiot
 
@@ -60,6 +60,13 @@ SUBSYSTEM=="gpio", MODE="0660", TAG+="uaccess"
             "\t$ sudo udevadm control --reload-rules\n"
             "\t$ sudo udevadm trigger", f"(md5: {rules_md5})")
 
+    @staticmethod
+    def check_credentials():
+        """Checks that we could write to some serial device, eg /dev/ttyACM0"""
+        print_check("Serial device write access", grp.getgrnam('dialout').gr_gid in os.getgroups(),
+                    "You may experience some write permission error('open(/dev/ttyACM0): Permission denied')\n"
+                    "Consider adding your user to the 'dialout' Linux group (sudo adduser `whoami` dialout)");
+
     def check(self):
         print_check('fastboot', which('fastboot'),
             "fastboot is used to flash the board. Please install the android "
@@ -67,6 +74,7 @@ SUBSYSTEM=="gpio", MODE="0660", TAG+="uaccess"
             "\thttps://developer.android.com/studio/releases/platform-tools")
         if platform.system() == 'Linux':
             self.check_udev_rules()
+            self.check_credentials()
 
 app_description = """
     AIoT configuration tool

commit 1d6fd799d8d73a4e7afa852850be6eb122079315

config: don't import grp on Windows platforms
On Windows, aiot-config fails with:
ModuleNotFoundError: No module named 'grp'

This is because the grp module is available for unix only [1].

Fix it by conditionally do the import.

Note that the usage of 'grp' is already done for linux-only.

[1] https://docs.python.org/3/library/grp.html
Fixes: 04ea06c ("Flashing documentation about the required Linux group")
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

aiot/config.py
@@ -5,7 +5,10 @@
 from pathlib import Path
 from shutil import which
 import hashlib
-import platform, os, grp
+import platform, os
+
+if platform.system() != "Windows":
+    import grp
 
 import aiot
 