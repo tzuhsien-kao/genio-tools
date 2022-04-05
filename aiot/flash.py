ab0ac6a508cb3edadade7b51729b1a45a4cc8af0
Alexandre Bailon <abailon@baylibre.com>
2022-04-05 13:31:45 +0200

    Add support of Download Agent Authentication
    This updates aiot-flash to support DAA.
    This adds three new options that could be used to enable
    DAA and select the bootstrap signature and the authentication file.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- aiot/flash.py
+++ aiot/flash.py
@@ -15,6 +15,8 @@ import aiot.image
 if platform.system() == 'Linux':
     import pyudev
 
+from os.path import exists
+
 def udev_wait():
     context = pyudev.Context()
     monitor = pyudev.Monitor.from_netlink(context)
@@ -118,6 +120,14 @@ class FlashTool(aiot.App):
         group.add_argument('--bootstrap', type=str, default='lk.bin',
             metavar='lk.bin',
             help='bootstrap binary used for flashing (default: lk.bin)')
+        group.add_argument('--bootstrap-sign', type=str, default='lk.sign',
+            metavar='lk.sign',
+            help='bootstrap binary signature used for flashing with DAA enabled (default: lk.sign)')
+        group.add_argument('--bootstrap-auth', type=str, default='auth_sv5.auth',
+            metavar='auth_sv5.auth',
+            help='authentication file used for flashing with DAA enabled (default: auth_sv5.auth)')
+        group.add_argument('--daa', action="store_true",
+            help="flash with DAA enabled")
         group.add_argument('--bootstrap-addr', type=int, default=0x201000,
             metavar='0x201000',
             help='Address where the bootstrap binary will be loaded (default: 0x201000)')
@@ -174,6 +184,15 @@ class FlashTool(aiot.App):
                 '--bootstrap-addr', hex(args.bootstrap_addr),
                 '--bootstrap-mode', args.bootstrap_mode,
             ]
+
+            if args.daa:
+               bootrom_app.extend(['-s', args.bootstrap_sign, '-t', args.bootstrap_auth])
+            else:
+               # By default, if '-s' or '-t' are not defined,
+               # bootrom_tool will try to use auth_sv5.auth and lk.bin.sign
+               # To avoid bootrom_tool from sending these files, pass invalid values for -s and -t
+               bootrom_app.extend(['-s', '', '-t', ''])
+
             try:
                 if platform.system() == 'Linux':
                     udev_wait()
