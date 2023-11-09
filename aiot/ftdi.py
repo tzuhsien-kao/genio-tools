6d6ed45350398e63354d36de2d35130d2ea98251
Pablo Sun <pablo.sun@mediatek.com>
2023-11-09 17:28:14 +0800

    ftdi: Support serial number in Linux board control
    - Add interface "print_device_list" to list device info, including
      serial to stdout.
    
    - find_device() now takes optional `serial` argument to open specific
      devices
    
    - The `new_serial` argument in the program() method is not implemented
      at the moment due to limitations of pyftdi library.
    
    Signed-off-by: Pablo Sun <pablo.sun@mediatek.com>

--- aiot/ftdi.py
+++ aiot/ftdi.py
@@ -7,10 +7,25 @@ import pyftdi.eeprom
 import usb
 
 class FtdiControl:
-    def __init__(self):
+    def __init__(self, serial = None):
         self.logger = logging.getLogger('aiot')
+        self.serial = serial
 
-    def find_device(self):
+    def print_device_list(self):
+        pyftdi.ftdi.Ftdi.show_devices()
+
+    def find_device(self, serial = None):
+        if serial:
+            for usb_dev, dev_idx in pyftdi.ftdi.Ftdi.list_devices():
+                if serial == usb_dev.sn:
+                    self.logger.debug(f"Matching serial found:{usb_dev}")
+                    return pyftdi.usbtools.UsbTools.get_device(usb_dev)
+            self.logger.error(f"Cannot find FTDI device with serial={serial}")
+            return None
+        else:
+            return self._find_device_by_usb()
+
+    def _find_device_by_usb(self):
         dev = list(usb.core.find(find_all=True, idVendor=0x0403))
         if len(dev) == 0:
             raise RuntimeError("Cannot find any FTDI device")
@@ -19,8 +34,8 @@ class FtdiControl:
 
         return dev[0]
 
-    def program(self, product_name, reset_gpio, download_gpio, power_gpio):
-        device = self.find_device()
+    def program(self, product_name, reset_gpio, download_gpio, power_gpio, new_serial = None):
+        device = self.find_device(self.serial)
 
         eeprom = pyftdi.eeprom.FtdiEeprom()
         eeprom.open(device)
@@ -36,7 +51,20 @@ class FtdiControl:
         eeprom.set_property(f"cbus_func_{reset_gpio}", "GPIO")
         eeprom.set_property(f"cbus_func_{download_gpio}", "GPIO")
 
+        if new_serial:
+            # Currently Linux version does not support serial setting.
+            # If we set serial, the following error occurs:
+            # pyftdi.ftdi.FtdiError: EEPROM does not support mirroring
+            # eeprom.set_serial_number(new_serial)
+            self.logger.error("Setting FTDI serial number is not supported")
+
         eeprom.commit(dry_run = False)
         eeprom.reset_device()
 
         self.logger.info("FTDI device programmed successfully")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    c = FtdiControl()
+    c.print_device_list()
+    # c.program("", 1, 2, 0)
