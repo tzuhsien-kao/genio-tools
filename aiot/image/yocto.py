tzuhsien-kao/genio-tools

commit 43bbe2a26a03acdb49af38cc8828b6fcac598ced

ubootenv: Add serialno support for Ubuntu images
Add support for `--serialno` for Ubuntu images. This allows customizing
the system serial number reported by the `dmidecode` tool.

Extract argument process from each image type (android, ubuntu, yocto)
and add common arguments when initializing the UbootEnv instance.

aiot/image/yocto.py
@@ -230,9 +230,6 @@ class YoctoImage:
         env = aiot.UBootEnv(env_size,
                             f"{self.path}/u-boot-initial-env")
 
-        if self.args.serialno:
-            env.add("serial#", self.args.serialno)
-
         env.update_env_list(self.args.uboot_env_set)
 
         if len(self.kernel_dtbo_autoload) > 0:

aiot/image/android.py
@@ -44,12 +44,9 @@ class AndroidImage:
         self.generate_uboot_env()
 
     def generate_uboot_env(self):
-        env = aiot.UBootEnv(8192, f"{self.path}/u-boot-initial-env")
-        if self.args.dtbo_index:
-            env.update("dtbo_index", self.args.dtbo_index)
-        if self.args.serialno:
-            env.update("serial#", self.args.serialno)
-
+        env = aiot.UBootEnv(8192,
+                            f"{self.path}/u-boot-initial-env",
+                            self.args)
         env.write_binary(f"{self.path}/u-boot-env.bin")
 
     @classmethod

aiot/image/ubuntu.py
@@ -143,7 +143,9 @@ class UbuntuImage:
             mbr.write(struct.pack("<I", hdr_crc32))
 
     def generate_uboot_env(self):
-        env = aiot.UBootEnv(int(self.uboot_env_size), f"{self.path}/u-boot-initial-env")
+        env = aiot.UBootEnv(int(self.uboot_env_size),
+                            f"{self.path}/u-boot-initial-env",
+                            self.args)
         env.gen_mac_addr(self.eth_oui, self.num_of_eth)
         env.update_env_list(self.args.uboot_env_set)
         if not self.uboot_env_redund_support:

aiot/ubootenv.py
@@ -8,12 +8,17 @@ import zlib
 import struct
 
 class UBootEnv:
-    def __init__(self, env_size, env_file):
+    def __init__(self, env_size, env_file, args):
         self.logger = logging.getLogger('aiot')
         self.env = [];
         self.env_size = env_size
+        self.args = args
         with open(env_file, "r") as env:
             self.env = env.readlines()
+        if self.args.dtbo_index:
+            self.add("dtbo_index", self.args.dtbo_index)
+        if self.args.serialno:
+            self.add("serial#", self.args.serialno)
 
     def add(self, name, value):
         self.env.append("{}={}".format(name, value))

commit f94e36c3685922e315daa318019b31def934509d

image: yocto: Fix flash failure
Fix fail to flash issue introduced in commit
43bbe2a ("ubootenv: Add serialno support for Ubuntu images")

Signed-off-by: Pablo Sun <pablo.sun@mediatek.com>

aiot/image/yocto.py
@@ -228,7 +228,8 @@ class YoctoImage:
         else:
             env_size = self.detect_uboot_env_size()
         env = aiot.UBootEnv(env_size,
-                            f"{self.path}/u-boot-initial-env")
+                            f"{self.path}/u-boot-initial-env",
+                            self.args)
 
         env.update_env_list(self.args.uboot_env_set)
 